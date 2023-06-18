34a3bb526eabece8e72c853c776fc7a230f9851c
Harini Mohan <128628820+kaizenchan@users.noreply.github.com>
2023-06-18 23:20:37 +0530

    Create 50_Pow(x,n).cpp

---
+++ 50_Pow(x,n).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double binaryExpo(double x, long long n){
+        if(n==0)
+            return 1;
+        if(n<0)
+            return 1.0/binaryExpo(x,-1*n);
+        if(n%2==0)
+            return myPow(x*x,n/2);
+        else
+            return(x*myPow(x*x,(n-1)/2));
+    }
+    double myPow(double x, int n) {
+        return binaryExpo(x,(long long)n);
+    }
+};
